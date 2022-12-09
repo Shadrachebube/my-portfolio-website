96ba6659dc457ee9c9ada76881d846400c42ef77
Shadrach Ebube <101437203+Shadrachebube@users.noreply.github.com>
2022-12-09 11:50:26 -0800

    added new file
    pleases check the live demo here.
    i didn`t upload all files here for security purpose

---
+++ testimonials.js
@@ -0,0 +1,29 @@
+const testimonials = [
+    {
+      quote: "<b>Shadrach Ebube</b> has been a wonderful Student for our organization before his passing out and till date. His work building a new website for us at the <b> 2022 Digitmoni International Tech-Submit, University of Nigeria Nsukka </b>resulted in growth across the organization, including but not limited to increased student applications and ticket sales. I hope I'll have the chance to continue working with Shadrach`s Team for years to come.",
+      avatar: "e9",
+      name:"Dominion Ndubuisi",
+      title: "CEO, DigitMoni Fintech Solutions & <br>DAST Technologies" 
+    },
+
+    {
+      quote: "The staff at <b>Ebube</b> are a breeze to work with: responsive, respectful, and talented; they care as much about our brand as we do. <br>Their  Work is Awesome! ",
+      avatar: "e3",
+      name:"Kevin Chubuoyim",
+      title: "Co-Founder BlockchainUNN <br>Founder & CEO GIDA" 
+    },
+
+    {
+      quote: "I have worked with the team at <b>Ebube</b> for several years now on a wide range of projects, ranging from <b>web design</b> and <b> maintenance to database development</b>. In each of these projects, they always worked quickly to fulfill our needs. I can always count on them to throughly research the problem and provide us a solution.",
+      avatar: "e13",
+      name:"Trust Darlinton",
+      title: "Managing Director, FIBA Technologies Inc" 
+    },
+
+    {
+      quote: "We worked with <b>Ebube</b> on a major <b> web and brand redesign project</b>, and I highly recommend them without hesitation. They are very <i> talented, thorough, and professional--full of creative ideas and timely in execution</i>. They came up with exactly the design concept we were looking for, producing a website far more appealing, user-friendly and secure than our previous site.<b>Ebube</b> is great to work with, and we expanded the range of projects we originally planned to do with them, including new e-newsletter template designs. Next up for us with <b>Ebube</b> is Phase Two of <b>our web development</b> . Can't wait!",
+      avatar: "e14",
+      name:"Shalom Flames",
+      title: "Former M.D, Bowdoin International Music Festival" 
+    },
+]
